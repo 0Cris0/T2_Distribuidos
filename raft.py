5f30b06018a349aada7184c8731c51d043afb164
0Cris0 <cristbal.albornoz@uc.cl>
2025-10-06 13:37:48 -0300

    Avance lun6 13:37

--- raft.py
+++ raft.py
@@ -33,7 +33,15 @@ def mayoria_lider(n_aceptan, n_nodos):
         return "elegido"
     return "no elegido"
 
+def aumentar_term(nodos, nodo_name, posible_valor_mayor):
+    nodo = nodos[nodo_name]
+    if(nodo["term"] < posible_valor_mayor):
+        nodo["term"] = posible_valor_mayor
+    return nodos
+
 def votar_por_lider(nodos, candidato):
+    activos = {k: v for k, v in nodos.items() if v.get('activo')}
+    #print(activos, "===========")
     aceptan = 1
     rechazan = 0
     # TODO: Implementar check si hay mayoría posible o no, cosa de invalidar votación
@@ -45,24 +53,25 @@ def votar_por_lider(nodos, candidato):
         if(nodo != candidato and nodo["activo"] != False):
             # Si ya votó en el term actual, vota rechazo
             if(nodo["ultimo_term_votado"] == term_candidato):
-                # print(f"        - El nodo {nodo["name"]} vota RECHAZO, porque ya votó este term")
+                #print(f"        - El nodo {nodo["name"]} vota RECHAZO, porque ya votó este term")
                 rechazan += 1
-                if(term_candidato > nodo["term"]): #TODO: Duda si esto está bien
-                    nodo["term"] = term_candidato
+                nodos = aumentar_term(nodos, nodo["name"], term_candidato)
+                nodos = aumentar_term(nodos, candidato["name"], nodo["term"])
                 continue
             # Si el term del candidato es menor que votante, vota rechazo
             if(nodo["term"] > term_candidato):
-                # print(f"        - El nodo {nodo["name"]} vota RECHAZO, porque tiene un term mayor")
+                #print(f"        - El nodo {nodo["name"]} vota RECHAZO, porque tiene un term mayor")
                 nodo["ultimo_term_votado"] = term_candidato #TODO: Duda si esto está bien
                 rechazan += 1
+                nodos = aumentar_term(nodos, candidato["name"], nodo["term"])
                 continue
             # Si votante no tiene logs, vota a favor
             if(len(nodo["logs"])==0):
-                # print(f"        - El nodo {nodo["name"]} vota APRUEBO, porque no tiene Logs")
+                #print(f"        - El nodo {nodo["name"]} vota APRUEBO, porque no tiene Logs")
                 aceptan+=1
                 nodo["ultimo_term_votado"] = term_candidato
-                if(term_candidato > nodo["term"]): #TODO: Duda si esto está bien
-                    nodo["term"] = term_candidato
+                nodos = aumentar_term(nodos, nodo["name"], term_candidato)
+                nodos = aumentar_term(nodos, candidato["name"], nodo["term"])
                 continue
             else:
                 ultimo_log_candidato = candidato["logs"][-1]
@@ -70,26 +79,26 @@ def votar_por_lider(nodos, candidato):
                 condicion_largo_logs = (len(candidato["logs"]) >= len(nodo["logs"]))
                 # Si term del último log es term mayor que el último del actual, vota a favor
                 if(ultimo_log_candidato[1] > ultimo_log[1]):
-                    # print(f"        - El nodo {nodo["name"]} vota APRUEBO, candidato tiene último log con term mayor que votante")
+                    #print(f"        - El nodo {nodo["name"]} vota APRUEBO, candidato tiene último log con term mayor que votante")
                     aceptan+=1
                     nodo["ultimo_term_votado"] = term_candidato
-                    if(term_candidato > nodo["term"]): #TODO: Duda si esto está bien
-                        nodo["term"] = term_candidato
+                    nodos = aumentar_term(nodos, nodo["name"], term_candidato)
+                    nodos = aumentar_term(nodos, candidato["name"], nodo["term"])
                     continue
                 # Si el term del último log es igual, pero candidato tiene log >= largo
                 # Vota a favor
                 if(ultimo_log_candidato[1] == ultimo_log[1] and condicion_largo_logs):
-                    # print(f"        - El nodo {nodo["name"]} vota APRUEBO, candidato tiene último log con = term que votante y es más largo")
+                    #print(f"        - El nodo {nodo["name"]} vota APRUEBO, candidato tiene último log con = term que votante y es más largo")
                     aceptan+=1
                     nodo["ultimo_term_votado"] = term_candidato
-                    if(term_candidato > nodo["term"]): #TODO: Duda si esto está bien
-                        nodo["term"] = term_candidato
+                    nodos = aumentar_term(nodos, nodo["name"], term_candidato)
+                    nodos = aumentar_term(nodos, candidato["name"], nodo["term"])
                     continue
                 # En todo otro caso +
                 # Si el term del último log fue menor al del votante, voto rechazo
                 # if(ultimo_log_candidato[1] < ultimo_log[1]):
                 else:
-                    # print(f"        - El nodo {nodo["name"]} vota RECHAZO, porque term ultimo log candidato es menor que votante")
+                    #print(f"        - El nodo {nodo["name"]} vota RECHAZO, porque term ultimo log candidato es menor que votante")
                     rechazan += 1
                     nodo["ultimo_term_votado"] = term_candidato #TODO: Duda si esto está bien
                     if(term_candidato > nodo["term"]): #TODO: Duda si esto está bien
@@ -98,7 +107,7 @@ def votar_por_lider(nodos, candidato):
     return mayoria_lider(aceptan, len(nodos))
 
 def eleccion_lider(nodos: dict, lider: dict):
-    # print(" > Eligiendo líder")
+    #print(" > Eligiendo líder")
     nodos = enviar_heartbeat(lider, nodos)
     mayoria = False
     contador_activos = 0
@@ -109,30 +118,35 @@ def eleccion_lider(nodos: dict, lider: dict):
         if(contador_activos >= len(nodos)// 2 +1):
             mayoria = True
             break
+    if(contador_activos < len(nodos)// 2 +1):
+        #print(" >> [WARNING]: No hay suficientes nodos para mayoría")
+        return (lider, nodos)
+    
+    #print(nodos)
     while(lider == None and mayoria == True):
-        # print(nodos)
         for nodo in nodos.keys():
             nodo = nodos[nodo]
             if(nodo["activo"] == True):
-                # print(f" >> >Nodo {nodo["name"]}, ({nodo["t_actual"]}+1/{nodo["timeout"]})")
+                #print(f" >> >Nodo {nodo["name"]}, ({nodo["t_actual"]}+1/{nodo["timeout"]})")
                 nodo["t_actual"] +=1
                 if(nodo["t_actual"] == nodo["timeout"]):
-                    # print(f"    El nodo {nodo["name"]} cumplió su timeout: {nodo["timeout"]}")
+                    #print(f"    El nodo {nodo["name"]} cumplió su timeout: {nodo["timeout"]}")
                     nodo["term"] += 1
                     nodo["ultimo_term_votado"] = nodo["term"]
-                    # print("    Procediendo a votar")
-                    # # print(nodos)
-                    # # print("========================")
+                    #print("    Procediendo a votar")
+                    # #print(nodos)
+                    # #print("========================")
                     resultado = votar_por_lider(nodos, nodo)
-                    # print(f"        El resultado es: {resultado}")
+                    #print(f"        El resultado es: {resultado}")
                     if(resultado == "elegido"):
                         lider = nodo
-                        # print(f"    -- Lider actual: {lider["name"]}")
+                        #print(f"    -- Lider actual: {lider["name"]}")
                         nodo["t_actual"] = 0
                         nodos = enviar_heartbeat(lider, nodos)
                         break
                     else:
                         nodo["t_actual"] = 0
+    
     return (lider, nodos)
 
 def enviar_heartbeat(lider, nodos):
@@ -140,7 +154,7 @@ def enviar_heartbeat(lider, nodos):
         nodo = nodos[nodo]
         if(nodo != lider and nodo["activo"] == True):
             nodo["t_actual"] = 0
-    # # print(nodos)
+    # #print(nodos)
     return nodos
 
 def replicar(nodos, nodo_actual, logs_lider):
@@ -182,7 +196,7 @@ def consolidar(nodos, lider, ya_consolidadas):
         for accion in acciones_consolidadas:
             if(accion not in ya_consolidadas):
                 ya_consolidadas.append(accion)
-                # print(f"        + Consolidando acción '{accion[0]}'")
+                #print(f"        + Consolidando acción '{accion[0]}'")
                 f_aux.procesar_accion(accion[0])
         # Ahora toca borrar todo
         for nodo in nodos.keys():
@@ -193,20 +207,20 @@ def consolidar(nodos, lider, ya_consolidadas):
     return (lider, nodos, ya_consolidadas)
 
 def procesar_comandos_raft(nodos, lider, comando, linea, ya_consolidadas):
-    # print(f"- Procesando acción: ({comando}), contexto: {linea}")
+    #print(f"- Procesando acción: ({comando}), contexto: {linea}")
     if(lider == None):
-        # print(f"-- No hay líder, eligiendo uno")
+        #print(f"-- No hay líder, eligiendo uno")
         (lider, nodos) = eleccion_lider(nodos, lider)
-        # print(f"    - El lider elegido es: {lider}")
+        #print(f"    - El lider elegido es: {lider}")
 
     if(comando == "Send" and lider != None):
-        # print(f"-- SEND  '{linea[1]}' ------------------")
+        #print(f"-- SEND  '{linea[1]}' ------------------")
         term_lider = lider["term"]
         accion = linea[1]
         lider["logs"].append((accion, term_lider))
-        # print(f"    - El lider actual queda: {lider}")
+        #print(f"    - El lider actual queda: {lider}")
     elif(comando == "Stop"):
-        # print(f"-- STOP, {linea[1]} ------------------")
+        #print(f"-- STOP, {linea[1]} ------------------")
         name_nodo = linea[1]
         if(name_nodo in nodos):
             nodos[name_nodo]["activo"] = False
@@ -214,12 +228,12 @@ def procesar_comandos_raft(nodos, lider, comando, linea, ya_consolidadas):
                 lider = None
                 (lider, nodos) = eleccion_lider(nodos, lider)
     elif(comando == "Start"):
-        # print(f"-- START, {linea[1]} ------------------")
+        #print(f"-- START, {linea[1]} ------------------")
         name_nodo = linea[1]
         if(name_nodo in nodos):
             nodos[name_nodo]["activo"] = True
     elif(comando == "Spread" and lider != None):
-        # print(f"-- SPREAD a nodos '{linea[1]}'------------------")
+        #print(f"-- SPREAD a nodos '{linea[1]}'------------------")
         lista_nodos = linea[1]
         if(len(lista_nodos) != 0):
             for nodo in nodos.keys():
@@ -230,7 +244,7 @@ def procesar_comandos_raft(nodos, lider, comando, linea, ya_consolidadas):
                         nodos = replicar(nodos, nodo_name, lider["logs"])
             (lider, nodos, ya_consolidadas) = consolidar(nodos, lider, ya_consolidadas)
     elif(comando == "Log"):
-        # print(f"-- LOG, {linea[1]} ------------------")
+        #print(f"-- LOG, {linea[1]} ------------------")
         # (lider, nodos) = consolidar(nodos, lider)
         variable_log = linea[1].strip()
         valor_log = f_aux.bbdd.get(variable_log, "Variable no existe")
@@ -248,12 +262,12 @@ def armar_nodo(nodos, nombre_nodo, timeout):
         "logs": [],
         "name": nombre_nodo
     }
-    # # print(f"Nuevo nodo creado: ", nodos[nombre_nodo])
+    # #print(f"Nuevo nodo creado: ", nodos[nombre_nodo])
     return nodos
 
 def funcion_raft(tests: str) -> None:
     """ # Completar con tu implementación o crea más archivos y funciones
-    # print(f"Ejecutando Raft con {tests}")
+    #print(f"Ejecutando Raft con {tests}")
     """
     # El primer paso es leer el archivo de tests
     with open(tests, "r", encoding="utf-8") as f:
@@ -266,7 +280,7 @@ def funcion_raft(tests: str) -> None:
         procesamiento = f_aux.eliminar_comentarios(linea)
         if len(procesamiento) > 0:
             lineas_clear.append(procesamiento)
-            # # print(procesamiento)
+            # #print(procesamiento)
     
     # RAFT:
     # Primera línea: Nodos y timeouts
@@ -283,17 +297,17 @@ def funcion_raft(tests: str) -> None:
     # TODO:Ver que no haya problema con los espacios en blanco
     # nodos = f_aux.quitar_duplicados(nodos)
 
-    # print(nodos)
-    # print("============\n")
+    #print(nodos)
+    #print("============\n")
 
     # Líneas siguientes: Procesamiento y lectura de comandos
     for linea in range(1, len(lineas_clear)):
         linea = lineas_clear[linea].strip().split(";")
         comando = linea[0]
         (lider, nodos, ya_consolidadas) = procesar_comandos_raft(nodos, lider, comando, linea, ya_consolidadas)
-        """ # print(nodos)
-        # print("============\n") """
+        """ #print(nodos)
+        #print("============\n") """
 
     f_aux.escribir_logs("Raft", tests, f_aux.logs_bbdd)
 
-# funcion_raft("casos_Raft/test_02.txt")
+# funcion_raft("casos_Raft/test_03.txt")
